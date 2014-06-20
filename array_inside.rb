1ce9bcab1ec37a734e2e2217c74105f4e544919d
Kelly Pederson <Kelly.Pederson@gmail.com>
2014-06-20 13:28:11 -0600

    Second

---
+++ array_inside.rb
@@ -0,0 +1,14 @@
+array_1 = [1, 2, 3]
+array_2 = [3,2]
+array_3 = [2,4]
+
+ # given [3, 2] the program will return true.
+ # given [1, 2, 3] and [2, 4] the program will return false
+
+
+
+
+p (array_2-array_1) == []
+p (array_3-array_1) == []
+
+
